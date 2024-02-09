aa9e5141f3c982a6fb5a29ff596e9086c89999a6
Ethan Crann <ecrann@protonmail.com>
2024-02-09 16:24:02 -0330

    Added lerping on skull movement

--- src/components/Skull.tsx
+++ src/components/Skull.tsx
@@ -1,19 +1,26 @@
 import { OrbitControls, useGLTF } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
 import { useRef } from "react";
-import { Object3D } from "three";
+import { Matrix4, Object3D, Quaternion, Vector3 } from "three";
 import { degToRad, lerp } from "three/src/math/MathUtils.js";
 
 export const Skull = () => {
   const { scene, nodes }= useGLTF("/Skull_Sep.glb");
   const targetRot = useRef<number>(0);
   const meshRef = useRef<Object3D>();
+  const targetPos = useRef<Vector3>(new Vector3(0, 1, 0));
+  const rotMatrix = useRef<Matrix4>(new Matrix4());
+  const targetQuat = useRef<Quaternion>(new Quaternion(0,0,0,0));
 
   useFrame(({ pointer, viewport }) => {
     if (meshRef.current) {
       const x = pointer.x * viewport.width * 1.25;
       const y = pointer.y * viewport.height * 1.25;
-      meshRef.current.lookAt(x, y, 1);
+      targetPos.current.set(x, y, 1);
+      rotMatrix.current.lookAt(targetPos.current, meshRef.current.position, meshRef.current.up);
+      targetQuat.current.setFromRotationMatrix(rotMatrix.current);
+      meshRef.current.quaternion.slerp(targetQuat.current, 0.1);
+
       if (nodes.SkullBottom) {
         nodes.SkullBottom.rotation.x = lerp(nodes.SkullBottom.rotation.x, degToRad(targetRot.current), 0.4);
       }
